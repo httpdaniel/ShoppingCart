f5fdd7395f51fd293cb3bf92f66b8108805e7406
daniel <daniel.097@hotmail.com>
2018-01-05 00:22:04 +0000

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,17 +1,12 @@
 public class Address
 {
-    // instance variables - replace the example below with your own
     private String street;
     private String city;
     private int zip;
     private String country;
 
-    /**
-     * Constructor for objects of class Address
-     */
     public Address(String street, String city, int zip, String country)
     {
-        // initialise instance variables
         this.street = street;
         this.city = city;
         this.zip = zip;
